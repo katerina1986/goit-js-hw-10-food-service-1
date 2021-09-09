4ed3cbdc6e6e1a07fdaf9e34f8f5119fbf48a69d
Oleksandr Balitskiy <popingalov@gmail.com>
2021-09-09 22:28:50 +0300

    пффф

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import './sass/main.scss';
+import menu from './menu.json';
+
+import menuCard from './template/menu-list.hbs';
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
+
+const menuList = document.querySelector('.js-menu');
+const checkBodyTheme = document.body.classList;
+
+function buildMenu(item) {
+  return menuList.insertAdjacentHTML('beforeend', menuCard(item));
+}
+buildMenu(menu);
+// 123
+const input = document.querySelector('#theme-switch-toggle');
+checkBodyTheme.add(
+  localStorage.getItem('theme') === null ? Theme.LIGHT : localStorage.getItem('theme'),
+);
+if (checkBodyTheme.value === Theme.DARK) {
+  input.checked = true;
+}
+
+function changeTheme(event) {
+  if (event.target.checked) {
+    checkBodyTheme.replace(Theme.LIGHT, Theme.DARK);
+    localStorage.setItem('theme', Theme.DARK);
+    return;
+  }
+  checkBodyTheme.replace(Theme.DARK, Theme.LIGHT);
+  localStorage.setItem('theme', Theme.LIGHT);
+}
+
+input.addEventListener('change', changeTheme);
